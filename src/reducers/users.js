ef4f3cc83190b1840cd94eefc2321d1a828af50a
kwabena.adu-darkwa <kwabena.adu-darkwa@makeduconsult.com>
2020-06-11 08:35:24 +0000

    double action effect

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,32 @@
 import { RECEIVE_USERS } from '../actions/users'
+import { ADD_QUESTION_ANSWER } from '../actions/questions'
+import { ADD_QUESTION } from '../actions/questions'
 
-export default function questions (state = {}, action) {
+export default function users (state = {}, action) {
   switch(action.type) {
     case RECEIVE_USERS :
       return {
         ...state,
         ...action.users
+      }
+    case ADD_QUESTION :
+    return {
+      ...state,
+      [action.question.author]: {
+        ...state[action.question.author],
+        questions: state[action.question.author].questions.concat(action.question.id)
+      }
+    }
+    case ADD_QUESTION_ANSWER :
+      return {
+        ...state,
+       [action.authedUser]:{
+         ...state[action.authedUser],
+         answers: {
+           ...state[action.authedUser].answers,
+         [action.qid] : action.answer
+         }
+       }
       }
     default :
       return state
